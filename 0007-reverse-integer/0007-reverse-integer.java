6403471d262cb3394d58e96a74895e0cdce07f18
KNagaHarshitha <146173125+KNagaHarshitha@users.noreply.github.com>
2024-08-02 23:54:34 +0530

    Time: 0 ms (100%), Space: 40.7 MB (64.58%) - LeetHub

--- 0007-reverse-integer/0007-reverse-integer.java
+++ 0007-reverse-integer/0007-reverse-integer.java
@@ -1,16 +1,27 @@
-class Solution {
-    public int reverse(int x) {
-        int y =0;
+// class Solution {
+//     public int reverse(int x) {
+//         int y =0;
+//         while(x!=0){
+//             int lastDigit=x%10;
+//             int newResult=y*10+lastDigit;
+//             if((newResult-lastDigit)/10!=y){
+//                 return 0;
+//             }
+//             y=newResult;
+//             x/=10;
+//         }
+//         return y;
+        
+//     }
+// }
+
+class Solution{
+    public int reverse(int x){
+        long ans=0;
         while(x!=0){
-            int lastDigit=x%10;
-            int newResult=y*10+lastDigit;
-            if((newResult-lastDigit)/10!=y){
-                return 0;
-            }
-            y=newResult;
+            ans=ans*10+x%10;
             x/=10;
         }
-        return y;
-        
+        return (ans<Integer.MIN_VALUE||ans>Integer.MAX_VALUE?0:(int)ans);
     }
 }
